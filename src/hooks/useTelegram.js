8e565b7fd2eda7bf0b69c6e336913761f85cb744
SaleXPetROV <sanyanazarov212@gmail.com>
2024-08-19 09:45:23 +0300

    init

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -9,6 +9,7 @@ export function useTelegram() {
     const onToggleButton = () => {
       if(tg.MainButton.isVisible) {
         tg.MainButton.hide();
+      } else {
         tg.MainButton.show();
       }
     };
