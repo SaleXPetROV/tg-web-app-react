9c4a4a7484e0d853834b28166ba984086392770d
SaleXPetROV <sanyanazarov212@gmail.com>
2024-08-19 11:14:13 +0300

    init

---
+++ src/components/ProductList/ProductList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './ProductList.css'
+
+const ProductList = () => {
+    return (
+        <div className="product-list">
+            ProductList
+        </div>
+    );
+};
+
+export default ProductList;
